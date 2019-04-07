5d571685593f6dd2b38e136ca37c7dccbc379232
jeanP-tech <45560971+jeanP-tech@users.noreply.github.com>
2019-04-07 18:26:57 +0900

    Update [1546] 평균.c

--- [1546] 평균.c
+++ [1546] 평균.c
@@ -3,6 +3,7 @@
 예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50 / 70 * 100이 되어 71.43점이 된다.
 세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오. */
 
+// 처음 풀이
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -30,6 +31,34 @@ int main() {
 	}
 
 	printf("%.2f", ave / N);
+
+
+}
+
+// 수정 후
+#include <stdio.h>
+
+int main() {
+	int N; 
+	double M;
+
+	scanf("%d", &N);
+
+	double sum = 0.0;
+	double score[1000] = { 0, };
+
+	M = score[0];
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &score[i]);
+		if (score[i] > M) M = score[i];
+	}
+
+	for (int i = 0; i < N; i++) {
+		sum = sum + score[i] / M * 100;
+	}
+
+	printf("%.2lf", sum / N);
 
 
 }
