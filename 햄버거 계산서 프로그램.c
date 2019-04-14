6b32c26792e5ab3ed31e700d029182029a7f9a24
jeanP-tech <45560971+jeanP-tech@users.noreply.github.com>
2019-04-15 00:32:37 +0900

    Update 햄버거 계산서 프로그램.c

--- 햄버거 계산서 프로그램.c
+++ 햄버거 계산서 프로그램.c
@@ -1,48 +1,54 @@
 /* 햄버거는 4000원, 콜라는 1500원, 감자튀김은 2000원이고, 세 가지를 세트로 주문하면 6500원이라고 하자. 
-햄버거, 코라, 감자튀김의 수량을 입력받아서 최대한 세트로 구성하고 나머지는 단품으로 계산하도록 한다.
+햄버거, 콜라, 감자튀김의 수량을 입력받아서 최대한 세트로 구성하고 나머지는 단품으로 계산하도록 한다.
 예를 들어 햄버거 2, 콜라1, 감자튀김 3을 주문하면 계산은 세트 1, 햄버거 1, 감자튀김 2로 해야 한다*/
 
 #include <stdio.h>
 
 int main(void) {
 	int burger, coke, fries;
-	int min; // 최소값 넣어두는 변수
+	int min; // 최솟값 넣어두는 변수
 
-	printf("햄버거, 콜라, 감자튀김 개수: ");
-	scanf("%d %d %d", &burger, &coke, &fries);
+	printf("[햄버거 4000원, 감자튀김 2000원, 콜라 1500원, 세트 6500원]\n");
+	printf("햄버거 개수? ");
+	scanf("%d", &burger);
+	printf("감자튀김 개수? ");
+	scanf("%d", &fries);
+	printf("콜라 개수? ");
+	scanf("%d", &coke);
 
 	min = burger;
 
 	if (coke < burger) {
 		min = coke;
-		if (coke < fries) {
-			min = fries;
-		}
+		if (fries < coke) min = fries;
 	}
 	else {
-		if (fries < burger) {
-			min = fries;
-		}
+		if (fries < burger) min = fries;
 	}
+	
+	burger -= min;
+	fries -= min;
+	coke -= min;
 
-	printf("%d\n", min);
-
-	printf("상품명	단가	수량	금액\n");
+	printf("\n상품명	단가	수량	금액\n");
 	printf("세트	6500	%d	%d\n", min, min * 6500);
 
-	if (burger == min) {
-		if (fries != min) printf("감자튀김	2000	%d	%d\n", fries - min, fries * 2000);
-		else if (coke != min) printf("콜라	1500	%d	%d\n", coke - min, coke * 1500);
+	if (burger != 0) {
+		printf("햄버거	4000	%d	%d\n", burger, burger * 4000);
+		if(fries != 0) printf("감자튀김	2000	%d	%d\n", fries, fries * 2000);
+		else if(coke != 0) printf("콜라	1500	%d	%d\n", coke, coke * 1500);
 	}
-	else if (fries == min) {
-		if (burger != min) printf("햄버거	4000	%d	%d\n", burger - min, burger * 4000);
-		else if (coke != min) printf("콜라	1500	%d	%d\n", coke - min, coke * 1500);
+	else if (fries != 0) {
+		printf("감자튀김  2000	%d	%d\n", fries, fries * 2000);
+		if(burger != 0) printf("햄버거	4000	%d	%d\n", burger, burger * 4000);
+		else if (coke != 0) printf("콜라	1500	%d	%d\n", coke, coke * 1500);
 	}
-	else if (coke == min) {
-		if (burger != min) printf("햄버거	4000	%d	%d\n", burger - min, burger * 4000);
-		else if (fries != min) printf("감자튀김	2000	%d	%d\n", fries - min, fries * 1500);
+	else if (coke != 0) {
+		printf("콜라	1500	%d	%d\n", coke, coke * 1500);
+		if (burger != 0) printf("햄버거	4000	%d	%d\n", burger, burger * 4000);
+		else if (fries != 0) printf("감자튀김  2000	%d	%d\n", fries, fries * 2000);
 	}
 
-	printf("-------------------------------------");
-	//printf("합계				%d", min * 6500 + (burger + fries + coke) - 3*min);
+	printf("-------------------------------------\n");
+	printf("합계				%d", min * 6500 + burger * 4000 + fries * 2000 + coke * 1500);
 }
