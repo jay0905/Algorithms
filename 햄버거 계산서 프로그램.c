ce980857178f1c5be287e8eddd0be13071343453
jeanP-tech <45560971+jeanP-tech@users.noreply.github.com>
2019-04-17 23:36:49 +0900

    Update 햄버거 계산서 프로그램.c

--- 햄버거 계산서 프로그램.c
+++ 햄버거 계산서 프로그램.c
@@ -1,5 +1,5 @@
 /* 햄버거는 4000원, 콜라는 1500원, 감자튀김은 2000원이고, 세 가지를 세트로 주문하면 6500원이라고 하자. 
-햄버거, 콜라, 감자튀김의 수량을 입력받아서 최대한 세트로 구성하고 나머지는 단품으로 계산하도록 한다.
+햄버거, 코라, 감자튀김의 수량을 입력받아서 최대한 세트로 구성하고 나머지는 단품으로 계산하도록 한다.
 예를 들어 햄버거 2, 콜라1, 감자튀김 3을 주문하면 계산은 세트 1, 햄버거 1, 감자튀김 2로 해야 한다*/
 
 #include <stdio.h>
@@ -35,20 +35,14 @@ int main(void) {
 
 	if (burger != 0) {
 		printf("햄버거	4000	%d	%d\n", burger, burger * 4000);
-		if(fries != 0) printf("감자튀김	2000	%d	%d\n", fries, fries * 2000);
-		else if(coke != 0) printf("콜라	1500	%d	%d\n", coke, coke * 1500);
 	}
-	else if (fries != 0) {
+	if (fries != 0) {
 		printf("감자튀김  2000	%d	%d\n", fries, fries * 2000);
-		if(burger != 0) printf("햄버거	4000	%d	%d\n", burger, burger * 4000);
-		else if (coke != 0) printf("콜라	1500	%d	%d\n", coke, coke * 1500);
 	}
-	else if (coke != 0) {
+	if (coke != 0) {
 		printf("콜라	1500	%d	%d\n", coke, coke * 1500);
-		if (burger != 0) printf("햄버거	4000	%d	%d\n", burger, burger * 4000);
-		else if (fries != 0) printf("감자튀김  2000	%d	%d\n", fries, fries * 2000);
 	}
 
-	printf("-------------------------------------\n");
-	printf("합계				%d", min * 6500 + burger * 4000 + fries * 2000 + coke * 1500);
+	printf("--------------------------------\n");
+	printf("합계			%d", min * 6500 + burger * 4000 + fries * 2000 + coke * 1500);
 }
