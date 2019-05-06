e786bbab2d139e2209f491d538ab41611030ce41
jeanP-tech <45560971+jeanP-tech@users.noreply.github.com>
2019-05-07 01:27:52 +0900

    Update and rename @[2675] 문자열 반복.c to [2675] 문자열 반복.c

--- [2675] 문자열 반복.c
+++ [2675] 문자열 반복.c
@@ -5,18 +5,19 @@
 QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
 */
 
-// 틀렸다고 뜨는데 어디서 틀린지 모르겠음. 
+// 배열 크기 때문에 계속 틀렸다고 뜨던 문제.
+// s가 문자열이기 때문에 null이 들어가서 배열 크기를 21로 잡아준다
 #include <stdio.h>
 #include <string.h>
 
 int main(void) {
 	int t, r;
-	char s[20];
+	char s[21];
 
 	scanf("%d", &t);
 
 	for (int i = 0; i < t; i++) {
-		char p[160] = "";
+		char p[168] = "";
 		scanf("%d", &r);
 		scanf("%s", s);
 		for (int j = 0; j < strlen(s); j++) {
@@ -24,5 +25,27 @@ int main(void) {
 				p[r * j + k] = s[j];
 			}
 		} printf("%s\n", &p);
+	}
+}
+}
+
+// 다른 풀이
+// p라는 새 배열을 반드시 설정해야되는지 알았는데 아니어서 다시 풀이
+#include <stdio.h>
+
+int main(void) {
+	int t, r;
+	char s[20];
+
+	scanf("%d", &t);
+
+	for (int i = t; i > 0; i--) {
+		scanf("%d", &r);
+		scanf(" %s", s);
+		for (int j = 0; s[j] != '\0'; j++) {
+			for (int k = 0; k < r; k++) {
+				printf("%c", s[j]);
+			}
+		} printf("\n");
 	}
 }
