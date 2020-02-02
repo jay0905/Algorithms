6a548c94eb6429a5c036740a9e944916f57f019d
Yeojin Park <jeanP.tech@gmail.com>
2020-02-02 18:29:26 +0900

    Create [11053] 가장 긴 증가하는 부분 수열.py

---
+++ [11053] 가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().strip().split()))
+dp = [0 for i in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if arr[j] < arr[i] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+
+print(max(dp))
