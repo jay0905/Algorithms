f3a5c97a3ff1c0ae6d2cbbdf5babe7699868fa74
Yeojin Park <jeanP.tech@gmail.com>
2020-03-20 17:26:24 +0900

    Create [9095] 1, 2, 3 더하기.py

---
+++ [9095] 1, 2, 3 더하기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    dp = [0, 1, 2, 4]
+
+    for _ in range(4, n + 1):
+        dp.append(sum(dp[-3:]))
+
+    print(dp[n])
