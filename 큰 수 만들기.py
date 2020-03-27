151154720cc77fc7b6bfd0a12d5dd4bbfee3c188
Yeojin Park <jeanP.tech@gmail.com>
2020-03-27 16:51:07 +0900

    Create 큰 수 만들기.py

---
+++ 큰 수 만들기.py
@@ -0,0 +1,15 @@
+def solution(number, k):
+    stack = []
+    
+    for i, num in enumerate(number):
+        while stack and stack[-1] < num and k > 0:
+            stack.pop()
+            k -= 1
+        
+        if k == 0:
+            stack += number[i:]
+            break
+        
+        stack.append(num)
+
+    return number[:-k] if k > 0 else "".join(stack)
