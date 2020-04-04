54d6a5b204f3d2c89e73ca16b4efb62fe9ffbe61
Yeojin Park <jeanP.tech@gmail.com>
2020-04-05 00:00:00 +0900

    Create [1182] 부분수열의 합.py

---
+++ [1182] 부분수열의 합.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+n, s = map(int, input().strip().split())
+numbers = list(map(int, input().strip().split()))
+cnt = 0
+
+for i in range(1, n + 1):
+    combi = combinations(numbers, i)
+
+    for c in combi:
+        if sum(c) == s:
+            cnt += 1
+
+print(cnt)
