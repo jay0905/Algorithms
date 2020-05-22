f3253031cdb50f5f4c6f2249cc243df7ff3c2da9
Yeojin Park <jeanP.tech@gmail.com>
2020-05-22 16:54:50 +0900

    Update 게임 맵 최단거리.py

--- 게임 맵 최단거리.py
+++ 게임 맵 최단거리.py
@@ -1,26 +1,31 @@
 from collections import deque
 
-def can_go(x, y, N, M, maps):
-    return 0 <= x < N and 0 <= y < M and maps[x][y] == 1
+def movable(x, y, n, m, maps):
+    return 0 <= x < n and 0 <= y < m and maps[x][y] == 1  
 
-def solution(maps):
-    N = len(maps)
-    M = len(maps[0])
-    queue = deque()
-    queue.append((0, 0))
-    
+def bfs(n, m, maps, queue):
     while queue:
         x, y = queue.popleft()
-        
-        if (x, y) == (N-1, M-1):
+
+        if x == n - 1 and y == m - 1:
             return maps[x][y]
-        
+
         DELTAS = ((1, 0), (-1, 0), (0, 1), (0, -1))
         for dx, dy in DELTAS:
             next_x, next_y = x + dx, y + dy
             
-            if can_go(next_x, next_y, N, M, maps):
+            if movable(next_x, next_y, n, m, maps):
                 queue.append((next_x, next_y))
                 maps[next_x][next_y] = maps[x][y] + 1
-                                
+
     return -1
+
+def solution(maps):
+    n = len(maps)
+    m = len(maps[0])
+
+    start = (0, 0)
+    queue = deque([start])
+    answer = bfs(n, m, maps, queue)
+                
+    return answer
